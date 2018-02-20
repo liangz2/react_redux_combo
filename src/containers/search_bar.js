763fd5c077d9b58362d081b5ce50f4bc3a0feab7
Zhengyi Liang <zliang@atb.com>
2018-02-19 22:27:43 -0800

    Add more code to search bar feature

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,15 +5,22 @@ export default class SearchBar extends Component {
         super(props);
 
         this.state = { term: "" };
+
+        this.onInputChange = this.onInputChange.bind(this);
     }
 
     onInputChange(event) {
-        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        // We need to go and fetch weather data
     }
 
     render() {
         return (
-            <form className="input-group">  
+            <form onSubmit={this.onFormSubmit} className="input-group">  
                 <input 
                     placeholder="Get a five-day forcast in your favourite cities"
                     className="form-control"
