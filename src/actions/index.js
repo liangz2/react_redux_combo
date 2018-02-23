8bd8477523f80c699b136e7f9cc46e4a31899f2c
Zhengyi Liang <zliang@atb.com>
2018-02-22 20:10:24 -0800

    Working on search bar function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const API_KEY = 'eb229dd1e88005f90519c8aa8d200be0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
+    console.dir(`Request: ${request}`);
 
     return {
         type: FETCH_WEATHER,
